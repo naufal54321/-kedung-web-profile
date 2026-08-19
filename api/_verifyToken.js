5c540aad17fa93fb170282d3658d74d508e84c3c
unknown <muhammadnaufal3864@gmail.com>
2026-08-19 14:11:44 +0700

    update fitur

--- api/_verifyToken.js
+++ api/_verifyToken.js
@@ -2,7 +2,9 @@
 import { createRemoteJWKSet, jwtVerify } from 'jose'
 
 const PROJECT_ID = 'kedung-api-7eaed'
-const AUDIENCE = process.env.FIREBASE_API_KEY || 'AIzaSyC9Aw39v6eUNEnHyGhkt9x7NCNCoh34_oE'
+const PROJECT_NUMBER = '405425437612'
+const API_KEY = process.env.FIREBASE_API_KEY || 'AIzaSyC9Aw39v6eUNEnHyGhkt9x7NCNCoh34_oE'
+const AUDIENCES = [API_KEY, PROJECT_ID, PROJECT_NUMBER]
 const ISSUER = `https://securetoken.google.com/${PROJECT_ID}`
 
 const jwks = createRemoteJWKSet(
@@ -15,7 +17,7 @@ export async function verifyIdToken(idToken) {
   }
   const { payload } = await jwtVerify(idToken, jwks, {
     issuer: ISSUER,
-    audience: AUDIENCE,
+    audience: AUDIENCES,
     algorithms: ['RS256']
   })
   return { uid: payload.sub, email: payload.email }
