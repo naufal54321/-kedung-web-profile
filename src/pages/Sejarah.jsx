c553606dd1854d94335ad129bb16382c1452b23f
unknown <muhammadnaufal3864@gmail.com>
2026-08-31 09:43:16 +0700

    content: tambah Sejarah Bregodo Suryeng Laga di halaman Sejarah

--- src/pages/Sejarah.jsx
+++ src/pages/Sejarah.jsx
@@ -1,6 +1,6 @@
 import ProfilHero from '../components/Profil/ProfilHero';
 import SEO from '../components/SEO';
-import { FaLandmark, FaHistory, FaUsers } from 'react-icons/fa';
+import { FaLandmark, FaHistory, FaUsers, FaShieldAlt } from 'react-icons/fa';
 
 function Sejarah() {
   return (
@@ -52,6 +52,56 @@ function Sejarah() {
         </div>
 
         <div className="vm-card" data-aos="fade-up" data-aos-delay="200">
+          <div className="vm-icon vm-icon-misi">
+            <FaShieldAlt />
+          </div>
+          <h3 className="vm-title">Sejarah Bregodo Suryeng Laga</h3>
+          <p className="vm-text">
+            Bregodo Suryeng Laga karepto saking Kedung, Guwosari, Pajangan, Bantul.
+            Dusun Kedung meniko kalebet ing wewengkon Guwosari Kapanewon Pajangan
+            Bantul Ngayogyakarta Hadiningrat ingkang tasih nguri nguri tradisi luhur
+            budaya Jawi, inggih meniko kirab bregodo gunungan tetilaripun saking
+            Kraton Ngayogyakarta Hadiningrat.
+          </p>
+          <p className="vm-text">
+            Bregodo Suryeng Laga meniko kagem pralampito perang Kanjeng Sinuwun
+            Pangeran Diponegoro, utawi Pangeran Haryo Selarong Gusti Pangeran
+            Adinegoro wonten ing bumi Selarong. Perang Pangeran Diponegoro kasebat
+            &ldquo;Perang Gerilya&rdquo; utawi perang nyawijining rakyat Selarong
+            kaliyan pasukan Pangeran Diponegoro, saenggo saged ngawonaken pasukan
+            Hindia Belanda (VOC) dipun damel kocar kacir sinaoso pasukan Walandi
+            sampun ngginakaken taktik perang adu domba utawi Devide Et Impera. Ewo
+            semanten sedoyo prajurit Walandi tinggal gelanggang colong playu tebih
+            saking bumi Nuswantoro.
+          </p>
+          <p className="vm-text">
+            Rikolo semanten wonten ing Kedung menika dipun ginakaken kagem olah
+            keprajuritan pasukan berkuda Pangeran Diponegoro ingkang dipun pandegani
+            Senopati Surya Laga utawi Suryeng Laga. Wondene ing Goa Selarong dipun
+            ginakaken Pangeran Diponegoro kagem pesanggrahan selami mimpin Perang
+            Diponegoro.
+          </p>
+          <p className="vm-text">
+            Wandene Bregodo Suryeng Laga Dusun Kedung menika anggambaraken kerukunan
+            rakyat Kedung saged nyawiji punggawa lan rakyat mbasmi Korupsi, Kolusi,
+            Nepotisme saking bumi Guwosari. Kanthi menika rakyat Kedung sak iyek
+            sakeko kapti jumenengaken barisan bregodo prajurit segelar sepapan kanthi
+            iring-iringan asung upo karti gunungan ingkang kaimpun saking kulu wektu
+            hasil bumi Kedung ingkang awujud sayur-sayuran, buah-buahan lan
+            sakpiturutipun.
+          </p>
+          <p className="vm-text">
+            Wondene kawontenan ing Dusun Kedung menika gunung, alas gung liwang
+            liwung pereng, wadas lan lemah lempung. Rakyat Kedung rumaos remen saged
+            ngaturaken Bregodo wonten ing Selarong menika. Kawontenan ing Dusun Kedung
+            menika saged kasebat Kedung menika katitik saking mapinten-pinten sumber
+            toyo, salah setunggalipun Sumur Miring. Wondene Sumur Miring meniko
+            miturut salah setunggalipun abdi Pangeran Diponegoro, Sumur Miring menika
+            kadadean saking tapak sikil jaran Pangeran Diponegoro.
+          </p>
+        </div>
+
+        <div className="vm-card" data-aos="fade-up" data-aos-delay="300">
           <div className="vm-icon vm-icon-visi">
             <FaUsers />
           </div>
